ade67946b43588d4fc4a02d50fe447a9718cfc27
guillaume <guillaumesamtmann24@gmail.com>
2025-06-17 12:07:04 +0200

    mise en place ui livre

--- Fragments_of_Me_Dev/Assets/Script/Maisson/takeBook.cs
+++ Fragments_of_Me_Dev/Assets/Script/Maisson/takeBook.cs
@@ -3,11 +3,13 @@ public class takeBook : MonoBehaviour
 {
     public GameObject HeldLivre;
     public MonoBehaviour FirstPersonScript;
+    public GameObject bookPanel;
     void Update()
     {
         if(HeldLivre.activeInHierarchy)
         {
             FirstPersonScript.enabled = false;
+            bookPanel.SetActive(true);
             Cursor.lockState = CursorLockMode.Confined;
             Cursor.visible = true;
         }
